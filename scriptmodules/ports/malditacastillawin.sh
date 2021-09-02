a6e5a180de0b02c90b28b45c90d5604b494b4dd5
George McMullen <1425046+GeorgeMcMullen@users.noreply.github.com>
2021-09-02 01:18:45 -0700

    Create malditacastillawin.sh

---
+++ scriptmodules/ports/malditacastillawin.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="malditacastillawin"
+rp_module_desc="Maldita Castilla by Locomalito"
+rp_module_help=""
+rp_module_licence="CC https://locomalito.com/maldita_castilla.php"
+rp_module_section="exp"
+rp_module_flags="!all rpi4"
+
+function depends_malditacastillawin() {
+    if ! rp_isInstalled "wine" ; then
+        md_ret_errors+=("Sorry, you need to install the wine scriptmodule")
+        return 1
+    fi
+}
+
+function install_bin_malditacastillawin() {
+    echo "Downloading Maldita Castilla to $md_inst"
+    downloadAndExtract "https://locomalito.com/juegos/Maldita_Castilla.zip" "$md_inst"
+}
+
+function configure_malditacastillawin() {
+    local system="malditacastillawin"
+    local malditacastillawin="$romdir/wine/Maldita Castilla (Locomalito).sh"
+
+    #
+    # Add Maldita Castilla entry to Wine roms directory in Emulation Station
+    #
+    cat > "$malditacastillawin" << _EOFMC_
+#!/bin/bash
+xset -dpms s off s noblank
+cd "$md_inst"
+matchbox-window-manager &
+WINEDEBUG=-all LD_LIBRARY_PATH="/opt/retropie/supplementary/mesa/lib/" setarch linux32 -L /opt/retropie/emulators/wine/bin/wine '$md_inst/Maldita Castilla.exe'
+_EOFMC_
+    chown $user:$user "$malditacastillawin"
+    chmod a+x "$malditacastillawin"
+}
